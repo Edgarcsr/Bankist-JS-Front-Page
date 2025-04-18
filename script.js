d822145a791375b1cc134149237667bb1692c897
Edgarcsr <edgarseabra@outlook.com.br>
2025-04-17 21:10:45 -0300

    🪶 - Adicionado Scroll Suave

--- script.js
+++ script.js
@@ -36,68 +36,101 @@ document.addEventListener("keydown", function (e) {
 ////////////////////////////////
 ////////////////////////////////
 
-const header = document.querySelector(".header");
+// const header = document.querySelector(".header");
 
-const message = document.createElement("div");
-message.classList.add("cookie-message");
-// message.textContent =
-// "We use cookies for improved functionalitty and analytics.";
-message.innerHTML = `We use cookies for improved functionalitty and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
+// const message = document.createElement("div");
+// message.classList.add("cookie-message");
+// // message.textContent =
+// // "We use cookies for improved functionalitty and analytics.";
+// message.innerHTML = `We use cookies for improved functionalitty and analytics. <button class="btn btn--close-cookie">Got it!</button>`;
 
-// header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(1));
+// // header.prepend(message);
+// header.append(message);
+// // header.append(message.cloneNode(1));
 
-// Delete Elements
-document
-  .querySelector(".btn--close-cookie")
-  .addEventListener("click", function () {
-    // message.remove();
-    message.parentElement.removeChild(message);
-  });
+// // Delete Elements
+// document
+//   .querySelector(".btn--close-cookie")
+//   .addEventListener("click", function () {
+//     // message.remove();
+//     message.parentElement.removeChild(message);
+//   });
 
-// Styles
-message.style.backgroundColor = "#37383d";
-message.style.width = `120%`;
+// // Styles
+// message.style.backgroundColor = "#37383d";
+// message.style.width = `120%`;
 
-console.log(message.style.height); // Doesn't Work
-console.log(message.style.background); // Works because style sets inline element in HTML
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+// console.log(message.style.height); // Doesn't Work
+// console.log(message.style.background); // Works because style sets inline element in HTML
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
 
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height) + 30 + "px";
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height) + 30 + "px";
 
-document.documentElement.style.setProperty("--color-primary", "orangered");
+// document.documentElement.style.setProperty("--color-primary", "orangered");
 
-// Attributes
-const logo = document.querySelector(".nav__logo");
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
+// // Attributes
+// const logo = document.querySelector(".nav__logo");
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
 
-logo.alt = "Beautiful, minimalist logo";
+// logo.alt = "Beautiful, minimalist logo";
 
-// Non standard:
-console.log(logo.designer); //don't work, returns undefined
-console.log(logo.getAttribute.designer);
-logo.setAttribute("company", "Bankist");
+// // Non standard:
+// console.log(logo.designer); //don't work, returns undefined
+// console.log(logo.getAttribute.designer);
+// logo.setAttribute("company", "Bankist");
 
-console.log(logo.src); // Gets absolute path
-console.log(logo.getAttribute("src")); //Gets relative path
+// console.log(logo.src); // Gets absolute path
+// console.log(logo.getAttribute("src")); //Gets relative path
 
-const link = document.querySelector(".nav__link--btn");
-console.log(link.href);
-console.log(link.getAttribute("href"));
+// const link = document.querySelector(".nav__link--btn");
+// console.log(link.href);
+// console.log(link.getAttribute("href"));
 
-// Data attributes
-console.log(logo.dataset.versionNumber);
+// // Data attributes
+// console.log(logo.dataset.versionNumber);
 
-// Classes
-logo.classList.add("c");
-logo.classList.remove("c");
-logo.classList.toggle("c");
-logo.classList.contains("c"); // Not includes
+// // Classes
+// logo.classList.add("c");
+// logo.classList.remove("c");
+// logo.classList.toggle("c");
+// logo.classList.contains("c"); // Not includes
 
-// Don't use! Overwrite content
-logo.className = "jonas";
+// // Don't use! Overwrite content
+// logo.className = "jonas";
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current Scroll pos", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "Document Client Height/Width",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  //  Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // Smooth Scroll
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+
+  // Modern Way
+  section1.scrollIntoView({ behavior: "smooth" });
+});
