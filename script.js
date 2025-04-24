ef9aa19b4c8da7847d0fd0f400c1382192bc84be
Edgarcsr <edgarseabra@outlook.com.br>
2025-04-24 15:39:10 -0300

    👀 - Observer API

--- script.js
+++ script.js
@@ -139,18 +139,52 @@ const handleHover = function (e) {
 };
 
 // Passing 'argument' to a handler
+
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 
 nav.addEventListener("mouseout", handleHover.bind(1));
 
-const initialCords = section1.getBoundingClientRect();
+// Ineficient way (on every scroll, it checks, what may overload the system):
 
-// Sticky Navigation
-window.addEventListener("scroll", function (e) {
-  console.log(window.scrollY);
-  if (window.scrollY > initialCords.top) nav.classList.add("sticky");
+// const initialCords = section1.getBoundingClientRect();
+
+// // Sticky Navigation
+// window.addEventListener("scroll", function (e) {
+//   console.log(window.scrollY);
+//   if (window.scrollY > initialCords.top) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// });
+
+// Sticky with Intersection Observer API
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach((entry) => {
+
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+const header = document.querySelector(".header");
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries; // = entries[0]
+  if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
 
 ////////////////////////////////
 ////////////////////////////////
