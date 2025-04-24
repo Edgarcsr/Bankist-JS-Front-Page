e542515b7a3bd94d8afd52b23b2c643c07695787
Edgarcsr <edgarseabra@outlook.com.br>
2025-04-24 15:56:22 -0300

    🖱️ - Animação ao 'Scrollar'

--- script.js
+++ script.js
@@ -186,6 +186,27 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+// Reveal Sections
+
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserver(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
 ////////////////////////////////
 ////////////////////////////////
 
